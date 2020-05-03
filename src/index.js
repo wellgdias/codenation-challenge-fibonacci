c40e2225da4796aadd2f73fc8939b9500486b93c
Wellington Dias <wads90@gmail.com>
2020-05-03 18:34:34 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 "use strict";
 
 const fibonacci = (maxValue = 350) => {
-  const sequence = [0, 1];  
+  const sequence = [0, 1];
+  let nextSequence;  
   while (sequence[sequence.length - 1] <= maxValue) {
-    sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+    nextSequence = sequence[sequence.length - 1] + sequence[sequence.length - 2]
+    sequence.push(nextSequence);
   }
 
   return sequence;
